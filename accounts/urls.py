787305a283016c691301c95988dac4382300053e
matt-stanford <mjfstanford@gmail.com>
2020-05-27 22:01:19 +0100

    added login via facebook

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,5 +8,4 @@ urlpatterns = [
     path('register', views.register, name='register'),
     path('login', auth_views.LoginView.as_view(), name='login'),
     path('logout', auth_views.LogoutView.as_view(), name='logout'),
-    path('google_login', TemplateView.as_view(template_name='registration/login_with_google.html')),
 ]
